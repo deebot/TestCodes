4715d64b2d2cc95376f9c76f8a732e51f1fdb938
Deepankar Maithani <deepankar19910@gmail.com>
2018-01-17 07:41:24 -0700

    Adding code outline and documentation for C1M1 Assignment

--- stats.c
+++ stats.c
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (C) 2017 by Alex Fosdick - University of Colorado
+ * Copyright (C) 2017 by Deepankar Maithani - University of Delhi
  *
  * Redistribution, modification or use of this software in source or binary
  * forms is permitted as long as the files maintain this copyright. Users are 
@@ -9,13 +9,14 @@
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file <stats.c> 
+ * @brief < This code displays the statastic about input numbers >
  *
- * <Add Extended Description Here>
+ * <The code prompts the user to enter 5 numbers than it sorts the numbers using bubble swap algorithm. Further it also claculates
+    mean median,maximum and minimum number >
  *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * @author <Maithani Deepankar>
+ * @date <15 january 2018 >
  *
  */
 
@@ -24,16 +25,10 @@
 #include <stdio.h>
 #include "stats.h"
 
-/* Size of the Data Set */
-#define SIZE (40)
 
 void main() {
 
-  unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
-                              114, 88,   45,  76, 123,  87,  25,  23,
-                              200, 122, 150, 90,   92,  87, 177, 244,
-                              201,   6,  12,  60,   8,   2,   5,  67,
-                                7,  87, 250, 230,  99,   3, 100,  90};
+  
 
   /* Other Variable Declarations Go Here */
   /* Statistics and Printing Functions Go Here */
@@ -41,3 +36,35 @@ void main() {
 }
 
 /* Add other Implementation File Code Here */
+float find_mean(float *ptr, float count)
+{
+
+
+
+}
+float sort_array(float *ptr,float count)
+{
+
+
+}
+float find_minimum(float *ptr, float count)
+{
+
+
+}
+
+float find_maximum(float *ptr, float count)
+{
+
+}
+float find_median(float *ptr, float count)
+{
+
+
+}
+
+int print_statastics(float val1,val2,val4,val5,*ptr)
+{
+
+
+}
--- stats.h
+++ stats.h
@@ -9,32 +9,88 @@
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file <stats.h> 
+ * @brief <This file has function prototypes  >
  *
- * <Add Extended Description Here>
+ * <Nil>
  *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * @author <MAITHANI DEEPANAKR>
+ * @date <15 JAN 2018 >
  *
  */
-#ifndef __STATS_H__
-#define __STATS_H__
+
+#define HOW_MANY (5)
 
 /* Add Your Declarations and Function Comments here */ 
+/**
+ * @brief <this function calculates mean>
+ *
+ * <this function will add up all the input numbers and will divide them with number of inputs>
+ *
+ * @param <*ptr> <pointer for array holding the input numbers>
+ * @param <count> <count of numbers given as input>
+ * @return <returns a float vlaue which represnets mean >
+ */
+float find_mean(float *ptr, float count)
+
 
 /**
- * @brief <Add Brief Description of Function Here>
+ * @brief <This function sorts the array >
  *
- * <Add Extended Description Here>
- *
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- *
- * @return <Add Return Informaiton here>
+ * <NIL>
+ * @param <*ptr> <pointer for array holding the input numbers>
+ * @param <count> <count of numbers given as input>
+ * @return <returns an array which has sorted numbers in decending order >
  */
+float sort_array(float *ptr,float count)
+
+
+/**
+ * @brief <This function finds minmum number >
+ *
+ * <NIL>
+ * @param <*ptr> <pointer for array holding the input numbers>
+ * @param <count> <count of numbers given as input>
+ * @return <returns a float type minium number >
+ */
+float find_minimum(float *ptr,float count)
+
+
+
+/**
+ * @brief <This function finds minmum number >
+ *
+ * <NIL>
+ * @param <*ptr> <pointer for array holding the input numbers>
+ * @param <count> <count of numbers given as input>
+ * @return <returns a float type maximum number >
+ */
+float find_maximum(float *ptr,float count)
+
+
+/**
+ * @brief <This function finds the median number >
+ *
+ * <NIL>
+ * @param <*ptr> <pointer for array holding the input numbers>
+ * @param <count> <count of numbers given as input>
+ * @return <returns a float type number which reprents median >
+ */
+float find_median(float *ptr,float count)
+
+
+/**
+ * @brief <This function prints the statastics about the entered numbers >
+ *
+ * <NIL>
+ * @param <*ptr> <pointer for array holding the input numbers>
+ * @param <count> <count of numbers given as input>
+ * @return <returns multiple values >
+ */
+
+int print_statastics(float val1,float val2,float val4,float val5,float *ptr)
+
+
 
 
 #endif /* __STATS_H__ */
